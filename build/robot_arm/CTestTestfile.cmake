c5c7c4f7c5b4eae0ad9b1282403684d15f795858
Dolan Nimrod <dolannimrod@gmail.com>
2025-04-25 23:41:18 +0300

    created a launch file and edited the cmake text file

---
+++ build/robot_arm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nimrod/robot_arm/src/robot_arm
+# Build directory: /home/nimrod/robot_arm/build/robot_arm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
